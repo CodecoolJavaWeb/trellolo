d275dd677d819dcbf02939cce1fb1fffa772d1b6
Szwajcii <d.szwajkos@gmail.com>
2018-09-12 11:34:46 +0200

    Implement drag and drop functions.

--- NoteController.js
+++ NoteController.js
@@ -16,9 +16,37 @@ export class NoteController{
         localStorage.save();
     }
 
-    handleDragStart(element){
-        
-    } 
+
+    handleDragStart(event) {
+        let shiftX = event.clientX - note.getBoundingClientRect().left;
+        let shiftY = event.clientY - note.getBoundingClientRect().top;
+
+        note.style.position = 'absolute';
+        note.style.zIndex = 100;
+
+        document.body.appendChild(note);
+
+        moveAt(event.pageX, event.pageY);
+
+        function moveAt(pageX, pageY) {
+            note.style.left = pageX - shiftX + 'px';
+            note.style.top = pageY - shiftY + 'px';
+        }
+
+        function onMouseMove(event) {
+            moveAt(evemt.pageX, event.pageY);
+        }
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        onMouseUp = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+        }
+    }
+
+    onDragStart = function() {
+        return false;
+    }
 
 
 }
