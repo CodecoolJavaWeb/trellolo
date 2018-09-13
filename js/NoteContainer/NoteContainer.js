49684311791da2f241884a98e2128454bbb39159
Senseinvader <d.ivantsov1983@gmail.com>
2018-09-13 11:10:57 +0200

    Add NoteContainer domain class

--- js/NoteContainer/NoteContainer.js
+++ js/NoteContainer/NoteContainer.js
@@ -1,7 +1,32 @@
 
 export class NoteContainer {
   constructor(id) {
-    this.list = new Array();
-    this.id = id;
+    this._list = new Array();
+    this._containerId = id;
   }
+
+//   static createContainerFromObject(object){
+//     let note = new Note();
+//     Object.assign(note, object);
+//     Object.assign(position, object._position)
+//     note.position = position;
+//     return note;
+// }
+
+  get list() {
+    return this._list;
+  }
+
+  set list(newList) {
+    this._list = newList;
+  }
+
+  get containerId() {
+    return this._containerId;
+  }
+
+  set containerId(newId) {
+    this._containerId = newId;
+  }
+
 }
